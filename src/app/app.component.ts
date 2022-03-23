558fbf5e7f5c8ad63cfd924f54a2f1fc61e60e8c
mecwerks <mecwerks@gmail.com>
2022-03-23 14:26:32 -0400

    first edition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'social-card';
+  socials = [
+    {
+      title: 'Twitch.tv',
+      icon: '',
+      url: 'https://twitch.tv/marxy_tv'
+    },
+    {
+      title: 'Twitter',
+      icon: '',
+      url: 'https://twitter.com/WherePantGo'
+    },
+    {
+      title: 'Youtube',
+      icon: '',
+      url: 'https://youtube.com/channel/UCKp9j5Vz3SBgGhZJLC5cpOg"'
+    }
+  ]
 }
